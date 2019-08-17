f10fe7481b03be4ac97e5f53c9000fdc8e502ae2
Andrea <andrea.velasquez.2019@upb.edu.co>
2019-08-17 12:11:42 +0200

    se realizo cambio ya que faltaron comillas

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-b=int(input("digite el primer numero:"))
-d=int(input("digite el segundo numero:"))
-mult=(b+b)
+a=int(input(digite el numero a:))
+b=int(input(digite el numero b:))
+c=int(input(digite el numero c:))
 import math
-raiz=(math.sqrt(d))
-print("el cuadrado del primer numero es:",mult)
-print("la raiz cuadrada del segundo numero es:",raiz)
+d=(math.sqrt((b*b)-4*a*c))
+x1=(-b+math.sqrt(d))/2*a)
+x2=((-b)/2*a)
+
+if d<0:
+  print("no existe solucion")
+else:
+   if d=0:
+     print("solucion:",x2)
+   else:
+       if d>0:
+         print("solucion:",x1)
+     
+     
+   
+
