6da02f5289db336cc97f3825d92a26695fcdc11f
Andrea <andrea.velasquez.2019@upb.edu.co>
2019-08-17 12:22:15 +0200

    se corrigio la alineacion de la line 12 y 16

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
-a=int(input(digite el numero a:))
-b=int(input(digite el numero b:))
-c=int(input(digite el numero c:))
+a=int(input("digite el numero a:"))
+b=int(input("digite el numero b:"))
+c=int(input("digite el numero c:"))
 import math
-d=(math.sqrt((b*b)-4*a*c))
-x1=(-b+math.sqrt(d))/2*a)
+d=math.sqrt((b*b)-4*a*c)
+x1=(-b+math.sqrt(d))/2*a
 x2=((-b)/2*a)
 
 if d<0:
   print("no existe solucion")
 else:
-   if d=0:
-     print("solucion:",x2)
-   else:
+    if d==0:
+       print("solucion:",x2)
+    else:
        if d>0:
-         print("solucion:",x1)
+        print("solucion:",x1)
      
      
    
